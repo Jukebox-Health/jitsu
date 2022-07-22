43d65a287c5a3872d1520c8c1a3cabe70ffddbc3
Dan Worth <djworth@jukeboxhealth.com>
2022-07-22 10:03:27 -0400

    Updating the customerio svg icon

--- configurator/frontend/catalog/src/destinations/lib/customerio.tsx
+++ configurator/frontend/catalog/src/destinations/lib/customerio.tsx
@@ -2,22 +2,18 @@ import { filteringExpressionDocumentation, modeParameter, tableName } from "./co
 import { stringType } from "../../sources/types"
 
 const icon = (
-    <svg
-        version="1.0"
-        xmlns="http://www.w3.org/2000/svg"
-        width="200.000000pt"
-        height="200.000000pt"
-        viewBox="0 0 200.000000 200.000000"
-        preserveAspectRatio="xMidYMid meet">
-        <g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)"
-            fill="#000000" stroke="none">
-            <path d="M1475 1179 c-22 -20 -40 -48 -49 -78 -20 -69 -60 -141 -98 -180 -159
-    -161 -402 -187 -583 -60 l-58 41 -99 -99 -99 -98 52 -47 c61 -55 186 -120 279
-    -145 44 -11 103 -17 180 -17 137 0 208 16 330 76 184 91 341 303 379 512 6 33
-    11 75 11 93 l0 33 -105 0 c-102 0 -105 -1 -140 -31z"/>
+    <svg height="100%"
+        width="100%" viewBox="0 0 157 111" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <title>customer-io-logo-vector</title>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="customer-io-logo-vector" transform="translate(0.700000, 0.800000)" fill-rule="nonzero">
+                <path d="M78,62.6 C95.3,62.6 109.3,48.6 109.3,31.3 C109.3,14 95.3,0 78,0 C60.7,0 46.7,14 46.7,31.3 C46.7,31.3 46.7,31.3 46.7,31.3 C46.8,48.6 60.8,62.6 78,62.6 C78,62.6 78,62.6 78,62.6 Z" id="Path" fill="#FFCD00"></path>
+                <path d="M78.2,78.2 L78.1,78.2 C56.7,78.2 38,63.7 32.7,42.9 C31.1,36.7 26,31.4 19.6,31.4 L0,31.4 C0,74.5 35,109.5 78.1,109.5 L78.1,109.5 L78.2,109.5 L78.2,78.2 Z" id="Path" fill="#00ECBB"></path>
+                <path d="M78,78.2 L78,78.2 C99.5,78.2 118.2,63.7 123.5,42.9 C125.1,36.7 130.2,31.4 136.6,31.4 L156.2,31.4 C156.2,74.5 121.2,109.5 78.1,109.5 L78,109.5 L78,78.2 Z" id="Path" fill="#AF64FF"></path>
+                <path d="M133.4,86.6 C102.9,117.1 53.4,117.1 22.9,86.6 C22.9,86.6 22.9,86.6 22.9,86.6 L45,64.5 C63.3,82.8 93,82.8 111.3,64.5 L133.4,86.6 Z" id="Path" fill="#7131FF"></path>
+            </g>
         </g>
     </svg>
-
 )
 
 const customerIODestination = {
@@ -34,7 +30,7 @@ const customerIODestination = {
     syncFromSourcesStatus: "not_supported",
     id: "customerio",
     type: "other",
-    displayName: "CustomerIO",
+    displayName: "Customer IO",
     defaultTransform: `// Code of CustomerIO transform:
 // https://github.com/jitsucom/jitsu/blob/master/server/storages/transform/customerio.js
 return toCustomerIO($)`,
